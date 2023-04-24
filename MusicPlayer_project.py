ab7123fedb5094811b9692296d27b3e23c1b3e83
Xaijones <Xjones6@students.kennesaw.edu>
2023-04-24 18:36:30 -0400

    Update MusicPlayer_project.py
    updated color, text and themes in project
    added a queue button
    also got pip working

--- MusicPlayer_project.py
+++ MusicPlayer_project.py
@@ -4,13 +4,13 @@ from tkinter.filedialog import askdirectory
 import os
 
 music_player = tkr.Tk()
-music_player.title("My Music Player")
-music_player.geometry("450x350")
+music_player.title("MUSIC BOT")
+music_player.geometry("550x450")
 directory = askdirectory()
 os.chdir(directory)
 song_list = os.listdir()
 
-play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
+play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='white', selectmode=tkr.SINGLE)
 for item in song_list:
     pos = 0
     play_list.insert(pos, item)
@@ -28,10 +28,13 @@ def pause():
     pygame.mixer.music.pause()
 def unpause():
     pygame.mixer.music.unpause()
+def queue():
+    pygame.mixer.music.queue()
 Button1 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PLAY", command=play, bg="green", fg="white")
 Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="STOP", command=stop, bg="red", fg="white")
 Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PAUSE", command=pause, bg="blue", fg="white")
 Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="UNPAUSE", command=unpause, bg="blue", fg="white")
+Button5 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="QUEUE", command=queue, bg="blue", fg="white")
 
 var = tkr.StringVar() 
 song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=var)
@@ -41,5 +44,6 @@ Button1.pack(fill="x")
 Button2.pack(fill="x")
 Button3.pack(fill="x")
 Button4.pack(fill="x")
+Button5.pack(fill="x")
 play_list.pack(fill="both", expand="yes")
 music_player.mainloop()
